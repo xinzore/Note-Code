7f5151679c0e13f42e2a02f4824ca322bf5d502f
xinzore <119423760+xinzore@users.noreply.github.com>
2026-01-18 12:10:17 +0300

    Add files via upload

--- api/lib/db.ts
+++ api/lib/db.ts
@@ -4,6 +4,10 @@ import * as schema from './schema.js';
 
 const { Pool } = pg;
 
+if (!process.env.DATABASE_URL) {
+    console.error('DATABASE_URL is not set!');
+}
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
